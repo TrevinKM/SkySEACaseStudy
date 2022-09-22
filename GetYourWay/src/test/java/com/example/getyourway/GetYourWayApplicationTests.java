0d9e5ac306468b72cbceb7aa7f63c27c5b1ceb0c
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-22 23:20:43 +0100

    Added dependencies for spring file

---
+++ GetYourWay/src/test/java/com/example/getyourway/GetYourWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getyourway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
