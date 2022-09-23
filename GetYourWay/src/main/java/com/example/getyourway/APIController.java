16b17c9b0167db968a011939b3566bf5ca1054f6
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-23 16:40:13 +0100

    request function

--- GetYourWay/src/main/java/com/example/getyourway/APIController.java
+++ GetYourWay/src/main/java/com/example/getyourway/APIController.java
@@ -1,6 +1,10 @@
 package com.example.getyourway;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +19,19 @@ public class APIController {
 
     private static String url = "https://aerodatabox.p.rapidapi.com/airports/search/location/51.488269/-0.326488/km/200/9";
 
-    
+
 
 
     @GetMapping("/locations")
-    public Object[] getLocations(){
-        Object[] locations = restTemplate.getForObject(url, Object[].class);
-        return locations;
+    public ResponseEntity<String> getLocations(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com");
+        headers.set("X-RapidAPI-Key", "a5f276c5d2msh12b6c646a5ffa06p1f3221jsnd5d923c9a7f6");
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.GET, requestEntity, String.class, "");
+        //Object[] locations = restTemplate.getForObject(url, Object[].class);
+        return response;
     }
 
 
