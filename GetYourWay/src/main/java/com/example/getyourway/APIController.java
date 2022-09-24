c146450a94d78395d7188ab1c3151abbe88b613d
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-24 12:31:14 +0100

    Seperated controller and service function

--- GetYourWay/src/main/java/com/example/getyourway/APIController.java
+++ GetYourWay/src/main/java/com/example/getyourway/APIController.java
@@ -14,24 +14,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class APIController {
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private APIService apiService;
 
     private static String url = "https://aerodatabox.p.rapidapi.com/airports/search/location/51.488269/-0.326488/km/200/9";
 
-
-
-
     @GetMapping("/locations")
     public ResponseEntity<String> getLocations(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com");
-        headers.set("X-RapidAPI-Key", "a5f276c5d2msh12b6c646a5ffa06p1f3221jsnd5d923c9a7f6");
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-                url, HttpMethod.GET, requestEntity, String.class, "");
-        //Object[] locations = restTemplate.getForObject(url, Object[].class);
-        return response;
+
+        return apiService.findFlightsNear();
     }
 
 
---
+++ GetYourWay/src/main/java/com/example/getyourway/APIService.java
@@ -0,0 +1,31 @@
+package com.example.getyourway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class APIService {
+
+    @Autowired
+    private RestTemplate template = new RestTemplate();
+
+    private static String url = "https://aerodatabox.p.rapidapi.com/airports/search/location/51.488269/-0.326488/km/200/9";
+
+
+    public ResponseEntity<String> findFlightsNear() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com");
+        headers.set("X-RapidAPI-Key", "a5f276c5d2msh12b6c646a5ffa06p1f3221jsnd5d923c9a7f6");
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = template.exchange(
+                url, HttpMethod.GET, requestEntity, String.class, "");
+        //Object[] locations = restTemplate.getForObject(url, Object[].class);
+        return response;
+    }
+
+}
