677ac832865b022f3300aa7f8bc9c3cb22937fe4
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-26 16:37:29 +0100

    Changes to controller

--- GetYourWay/src/main/java/com/example/getyourway/APIController.java
+++ GetYourWay/src/main/java/com/example/getyourway/APIController.java
@@ -16,15 +16,15 @@ public class APIController {
 
 
     @Autowired
-<<<<<<< HEAD
+
     private APIService apiService;
 
     //private static String url = "https://aerodatabox.p.rapidapi.com/airports/search/location/51.488269/-0.326488/km/200/9";
-=======
+
     private RestTemplate restTemplate;
 
     private static String url = "https://aerodatabox.p.rapidapi.com/airports/search/location/51.488269/-0.326488/km/200/9";
->>>>>>> origin/master
+
 
     @GetMapping("/locations")
     public ResponseEntity<String> getLocations(){
