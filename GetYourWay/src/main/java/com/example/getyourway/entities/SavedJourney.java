5b5b1719c15b6e29cdf2a614f81dc2d2ad2d0f9c
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-26 19:07:01 +0100

    making DTOs

--- GetYourWay/src/main/java/com/example/getyourway/entities/SavedJourney.java
+++ GetYourWay/src/main/java/com/example/getyourway/entities/SavedJourney.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 //TODO: Move to DTO
 @Entity
-public class Journey {
+public class SavedJourney {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
