251da7254b3eda3221191587520f2def70f9d8a2
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-27 15:33:40 +0100

    sample current weather at location

--- GetYourWay/src/main/java/com/example/getyourway/controller/WeatherController.java
+++ GetYourWay/src/main/java/com/example/getyourway/controller/WeatherController.java
@@ -1,7 +1,20 @@
 package com.example.getyourway.controller;
 
+import com.example.getyourway.service.APIService;
+import com.example.getyourway.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+    @GetMapping("/weather")
+    public ResponseEntity<String> getCurrentWeatherAt(){
+        return weatherService.findWeatherAt("Leeds");
+    }
+
 }
--- GetYourWay/src/main/java/com/example/getyourway/service/WeatherService.java
+++ GetYourWay/src/main/java/com/example/getyourway/service/WeatherService.java
@@ -11,21 +11,19 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherService implements IAPIService {
     private final String baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/";
     private final String key = "key=4H8VPJ5U2NCJXSLAFEC5R4AQ5";
+
     public ResponseEntity<String> findWeatherAt(String location) {
 
         //Parameterise this
-        final String url = baseUrl + "timeline/"+  location + "?unitGroup=metric&" + key;
+        final String url = baseUrl + "weatherdata/forecast?aggregateHours=24&contentType=json&unitGroup=us&locationMode=single&locations=leeds&" + key;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com");
-        headers.set("X-RapidAPI-Key", "a5f276c5d2msh12b6c646a5ffa06p1f3221jsnd5d923c9a7f6");
-
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(null);
         ResponseEntity<String> response = template.exchange(
                 url, HttpMethod.GET, requestEntity, String.class, "");
-        //Object[] locations = restTemplate.getForObject(url, Object[].class);
+
         return response;
     }
+
 
 
 }
