146ed2178a4f40604923a78c7090b7af8c07e7ef
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-27 14:28:00 +0100

    building weather at location  request

--- GetYourWay/src/main/java/com/example/getyourway/service/WeatherService.java
+++ GetYourWay/src/main/java/com/example/getyourway/service/WeatherService.java
@@ -9,7 +9,23 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService implements IAPIService {
-   private final String baseUrl = "";
+    private final String baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/";
+    private final String key = "key=4H8VPJ5U2NCJXSLAFEC5R4AQ5";
+    public ResponseEntity<String> findWeatherAt(String location) {
+
+        //Parameterise this
+        final String url = baseUrl + "timeline/"+  location + "?unitGroup=metric&" + key;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com");
+        headers.set("X-RapidAPI-Key", "a5f276c5d2msh12b6c646a5ffa06p1f3221jsnd5d923c9a7f6");
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = template.exchange(
+                url, HttpMethod.GET, requestEntity, String.class, "");
+        //Object[] locations = restTemplate.getForObject(url, Object[].class);
+        return response;
+    }
 
 
 }
