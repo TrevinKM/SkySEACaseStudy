e5fcd02d8a98c0323de828cbfb9c19d614737e9e
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-30 17:17:09 +0100

    Added key file

--- GetYourWay/src/main/java/com/example/getyourway/exceptions/ResourceNotFoundException.java
+++ GetYourWay/src/main/java/com/example/getyourway/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.getyourway.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "This user is not found")
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String e){
         super(e);
