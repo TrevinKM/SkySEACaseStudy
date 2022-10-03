14f8f098767b735af837113b9ab7d6f34be22420
Charlotte <cabbotts1@outlook.com>
2022-10-03 21:28:55 +0100

    initial commit for routing

--- GetYourWay/src/main/gywWebApp/reactjs/src/App.js
+++ GetYourWay/src/main/gywWebApp/reactjs/src/App.js
@@ -1,46 +1,69 @@
-import React from 'react';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-import './App.css';
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import NavBar from './components/NavBar';
+// import RecommendedDestinations from './components/RecommendedDestinations';
+// import Profile from './components/Profile';
+// import SignUp from './components/SignUp';
+// import LogIn from "./components/LogIn";
+// import TravelSearch from "./components/TravelSearch";
+// import SignUpForm1 from "./components/SignUpForm1";
+// import submitSignUpForm from "./components/SubmitSignUpForm1";
+// import SignUpForm from "./components/SignUpForm";
 
-import Header from "./components/Header";
+// function App() {
+//   return (
+//       <>
+//           <NavBar />
+//           <div className="content">
+//
+//
+//           </div>
+//           <Header />
+//           <main className="container">
+//             <Profile />
+//             <RecommendedDestinations />
+//               <SignUp />
+//               <LogIn />
+//               <TravelSearch />
+//               {/*<SignUpForm1 submitSignUpForm1={submitSignUpForm}/>*/}
+//               <SignUpForm submitSignUpForm={} />
+//
+//           </main>
+//           <Footer />
+//       </>
+//   );
+// }
+
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import {
+    Home,
+    About,
+    Profile,
+    RecommendedDestinations2,
+    TravelSearch,
+    NotFound404
+} from './pages';
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import NavBar from './components/NavBar';
-import RecommendedDestinations from './components/RecommendedDestinations';
-import Profile from './components/Profile';
-import SignUp from './components/SignUp';
-import LogIn from "./components/LogIn";
-import TravelSearch from "./components/TravelSearch";
 
 function App() {
-  return (
-      <>
-          <NavBar />
-          <div className="content">
-
-              {/*<BrowserRouter>*/}
-              {/*    <Routes>*/}
-              {/*        <Route path="/travelSearch" component={TravelSearch} />*/}
-              {/*        <Route path="/login" component={LogIn} />*/}
-              {/*        <Route path="/signup" component={SignUp} />*/}
-              {/*        <Route path="/profile" component={Profile} />*/}
-              {/*        <Route path="/recommendedDestinations" component={RecommendedDestinations} />*/}
-              {/*  </Routes>*/}
-              {/*</BrowserRouter>*/}
-
-          </div>
-          <Header />
-          <main className="container">
-            <Profile />
-            <RecommendedDestinations />
-              <SignUp />
-              <LogIn />
-              <TravelSearch />
-          </main>
-          <Footer />
-      </>
-  );
+    return (
+        <>
+        <NavBar />
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/recommendedDestinations" element={<RecommendedDestinations2 />} />
+                    <Route path="/travelSearch" element={<TravelSearch />} />
+                    <Route path="*" element={<NotFound404 />} />
+                </Routes>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
