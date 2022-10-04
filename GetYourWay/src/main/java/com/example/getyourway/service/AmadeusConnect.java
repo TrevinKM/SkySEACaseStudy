b759f35141697d96615eac3e514c2c6d0da6d7f1
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-10-04 10:00:07 +0100

    Added amadeus intergration

---
+++ GetYourWay/src/main/java/com/example/getyourway/service/AmadeusConnect.java
@@ -0,0 +1,22 @@
+package com.example.getyourway;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.referenceData.Locations;
+import com.amadeus.resources.Location;
+import com.amadeus.exceptions.ResponseException;
+s
+enum AmadeusConnect {
+    INSTANCE;
+    private Amadeus amadeus;
+    private AmadeusConnect() {
+        this.amadeus = Amadeus
+                .builder("YOUR_API_KEY", "YOUR_API_SECRET")
+                .build();
+    }
+    public Location[] location(String keyword) throws ResponseException {
+        return amadeus.referenceData.locations.get(Params
+                .with("keyword", keyword)
+                .and("subType", Locations.AIRPORT));
+    }
+}
