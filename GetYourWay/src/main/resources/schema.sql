116d759d9a593beb2c6969412c7965142a983a03
Ryan Walton <ryanwalton1999@gmail.com>
2022-10-04 21:10:44 +0100

    added test schema

--- GetYourWay/src/main/resources/schema.sql
+++ GetYourWay/src/main/resources/schema.sql
@@ -26,6 +26,11 @@ USE
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test`
+(
+    `firstname`       varchar(256) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `users`
 (
     `id`              int NOT NULL AUTO_INCREMENT,
