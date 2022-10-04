064dc7bec6b3f2aadb522379826755073ea52f54
Charlotte <cabbotts1@outlook.com>
2022-10-04 22:26:30 +0100

    addedSkyLogo

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/NavBar.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from "../logo.png";
 import skyLogo from "./skyLogo.png";
 import {Link} from 'react-router-dom';
 
@@ -8,7 +7,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">
-                    <img src={skyLogo} alt="Get Your Way logo" style={{width: '25px'}}
+                    <img src={skyLogo} alt="Get Your Way logo" style={{width: '50px'}}
 
                          className="d-inline-block align-text-top"/>
                     Get Your Way</a>
