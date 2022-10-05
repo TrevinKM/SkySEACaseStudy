731597cddd74c8e91e9a4629216543e79691713d
Charlotte <cabbotts1@outlook.com>
2022-10-05 16:00:26 +0100

    weather endpoint on UI

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/Profile.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/Profile.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import UserDetails from "./UserDetails";
-import SavedJourney from "./SavedJourney";
+
 
 const Profile = () => {
     return (
         <>
             <h3>Your Details</h3>
             <UserDetails/>
-            <h3>Your Saved Journeys</h3>
-            <SavedJourney/>
         </>
     );
 }
