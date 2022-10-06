46981f0bad551a44ea8b548265ce861fffaacaf0
Sarah Beckett <sarah.beckett@sky.uk>
2022-10-06 10:45:58 +0100

    tidied up homepage static destinations

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/Home.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/Home.jsx
@@ -8,24 +8,21 @@ export function Home() {
     return (
         <>
         <div>
-            <img
-                src={line}
-                alt="rainbow line"
-                style={{width: '1450px'}}
-            />
-            <h1>Welcome to</h1>
+           
+            <h3>Welcome to</h3>
             <Header />
-            <nav>
-                <Link to="about" >About</Link>
-                <Link to="entryPage">Entry Page</Link>
-                <Link to="recommendedDestinations" >Recommended Destinations</Link>
-                <Link to="profile">Profile</Link>
-                <Link to="travelSearch" >Travel Search</Link>
-            </nav>
+
         </div>
 
         <div>
             <BootstrapCarousel />
+        </div>
+        <div>
+        <img
+                src={line}
+                alt="rainbow line"
+                style={{width: '1450px'}}
+            />
         </div>
         </>
     );
