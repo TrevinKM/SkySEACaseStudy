f018c64dfd0b33b8a15312bb448680726ca58bfe
Ryan Walton <ryanwalton1999@gmail.com>
2022-10-07 16:01:37 +0100

    added errors

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/SignUp.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/SignUp.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import SignUpForm from "./SignUpForm";
 import {Col, Container, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 const SignUp = () => {
     return (
         <Container className={"mx-5"}>
             <Row >
                 <Col md={6}>
-                    <h3 className={"mb-3"}>Sign up</h3>
+                    <h3 className={"mb-3"}>Sign Up</h3>
                     <SignUpForm/>
+                    <p>Already have an account? <NavLink to={"/login"} >Log in here</NavLink></p>
                 </Col>
                 <Col>
                     <img className="smallimg" src={"images/8dest.jpeg"} style={{objectFit: "cover", width:"100%", height: "100%"}}/>
