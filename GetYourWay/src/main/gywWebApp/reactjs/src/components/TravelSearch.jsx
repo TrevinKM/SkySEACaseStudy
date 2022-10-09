60c32f53403ba2e7a87a7eb5a93e9fc7da8daaca
Sarah Beckett <sarah.beckett@sky.uk>
2022-10-09 12:00:29 +0100

    changed travel search

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/TravelSearch.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/TravelSearch.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import SearchBar from "./SearchBar";
-import {Container, Image} from "react-bootstrap";
+import {Container, Image, Card, Row, CardGroup} from "react-bootstrap";
+
 
 const TravelSearch = () => {
     const [origin, setOrigin] = useState();
     const [destination, setDestination] = useState();
     return (
         <>
-            <Container>
-
+            
+                
+                <Card>
                 <h2>Find Your Way</h2>
                 <p>
-                    Search for a destination or use your location and desired destination to begin your adventure.
+                    Search for a destination to begin your adventure.
                 </p>
-                <p>
+                </Card>
+                <CardGroup>
+                <Card> 
+                    <br/>
+                    <br/><br/><br/><br/><br/><br/>
                     <SearchBar />
-                </p>
-
-                <div className="text-center justify-content-md-center">
+                
+                </Card>
+               
+               
+            
+                <Card>
+                
                 <Image
                     src="/images/7dest.jpeg"
-                    style={{width: '70%'}}
+                    style={{width: '100%'}}
                     className="align-content-center"
+                
                 />
-            </div>
-            </Container>
+                </Card>
+                </CardGroup>
+            
+           
+           
         </>
     )
 };
