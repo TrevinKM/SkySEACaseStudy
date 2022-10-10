323df9bce2bf0282ce9aea7764779d16c373f953
shbeckett <98051500+shbeckett@users.noreply.github.com>
2022-10-10 11:04:14 +0100

    Update About.jsx
    More copy added

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/About.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
     return (
         <Container>
         <h1>About Get Your Way</h1>
+            <p>Get Your Way is a travel app that lets users plan journeys to destinations linked to content on the Sky platform. It offers logged-in users a single-page view of their journey along with local weather information.</p>
             <p>Get Your Way was created by The Increments as the project element of Sky's Graduate Software Engineering Academy Boot Camp in October 2022. The Increments are:
             </p>
             <br/>
