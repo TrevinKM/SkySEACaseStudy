5b3b3446a29cdda8ffc03ca6bfb5862f6a556f31
shbeckett <98051500+shbeckett@users.noreply.github.com>
2022-10-10 08:28:35 +0100

    Copy update for about section

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/About.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/About.jsx
@@ -6,8 +6,7 @@ const About = () => {
     return (
         <Container>
         <h1>About Get Your Way</h1>
-            <p>Get Your Way was created by The Increments in October 2022.<br/>
-            Below are profile cards of the members of The Increments.
+            <p>Get Your Way was created by The Increments as the project element of Sky's Graduate Software Engineering Academy Boot Camp in October 2022. The Increments are:
             </p>
             <br/>
         <AboutCards />
