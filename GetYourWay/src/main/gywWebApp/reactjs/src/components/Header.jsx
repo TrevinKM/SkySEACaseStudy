00842e5b53c002aae12af78f3fb6f4e94c739fac
Trevin <64080171+TrevinKM@users.noreply.github.com>
2022-10-10 11:04:24 +0100

    Update Header.jsx
    Added ID to header

--- GetYourWay/src/main/gywWebApp/reactjs/src/components/Header.jsx
+++ GetYourWay/src/main/gywWebApp/reactjs/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './BootstrapCarousel.css';
 
 const Header = () => {
     return (
         <header style={{paddingTop: '15px', paddingBottom: '15px', color: 'SlateGray'}}>
-            <h3 style={{color : 'DarkSlateGray'}}>Start your adventure here...</h3>
+            <h3 id={'grad-text'} >Start your adventure here...</h3>
             <p>Be inspired and browse through our
                 <a id="rd" href="/recommendedDestinations" ><strong style={{color : 'DarkSlateGray'}}> recommended destinations</strong> </a>
                 from Sky shows you love!
